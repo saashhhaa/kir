e79734c4f877ad5c919a5abf456ab810810a5803
Ver_s <aleksandra.bystrova@alatoo.edu.kg>
2026-08-08 23:50:47 +0600

    add github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(
+  {
+      base: '/kir/',
   plugins: [vue()],
 })
