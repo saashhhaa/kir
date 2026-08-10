75baef61b095acb2088beafe782a8b1d4824ece3
Ver_s <aleksandra.bystrova@alatoo.edu.kg>
2026-08-10 22:15:37 +0600

    chore:fix audio import

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -1,6 +1,6 @@
-import audio1 from '../assets/audio/Урал Гайсин - Священная война.mp3'
-import audio2 from '../assets/audio/Akira_Yamaoka_-_Love_Psalm_(SkySound7.com).mp3'
-import audio3 from '../assets/audio/Egor_i_Opizdenevshie_-_ZHeka_uzhe_v_Gamburge_(SkySound7.com).mp3'
+// import audio1 from '../assets/audio/Урал Гайсин - Священная война.mp3'
+// import audio2 from '../assets/audio/Akira_Yamaoka_-_Love_Psalm_(SkySound7.com).mp3'
+// import audio3 from '../assets/audio/Egor_i_Opizdenevshie_-_ZHeka_uzhe_v_Gamburge_(SkySound7.com).mp3'
 import audio4 from '../assets/audio/Порез На Собаке - Теперь Мы Снова Вдвоём.mp3'
 
 export const tracks = [
