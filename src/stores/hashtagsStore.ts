e79e02880d7955114f187276c6a74cb439ad79a7
Ver_s <aleksandra.bystrova@alatoo.edu.kg>
2026-08-12 22:33:54 +0600

    chore: дефолтным хэштегом назначен первый из таблицы (final pictures | f0l)

--- src/stores/hashtagsStore.ts
+++ src/stores/hashtagsStore.ts
@@ -5,13 +5,17 @@ import type { Hashtag } from '../types/hashtagType';
 
 export const useHashTagsStore = defineStore('hashtag', () => {
 const hashtags = ref<Hashtag[]>([])
-const currentHashId = ref<number | null>(null)
+const currentHashId = ref<number | null>()
 function setCurrHash(id:number | null){
     currentHashId.value = id
 }
 
 async function fetchHashtags () {
   hashtags.value = await getHashtags()
+  if(hashtags.value.length >0 ){
+     currentHashId.value = hashtags.value[0]!.id
+  }
+ 
 }
 
   return {
