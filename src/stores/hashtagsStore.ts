4cc63cba27032bd5b72daa4d557d278d0e7d7ad9
Ver_s <aleksandra.bystrova@alatoo.edu.kg>
2026-08-22 00:03:16 +0600

    chore: выбор хештеша и темы хранится сессионно

--- src/stores/hashtagsStore.ts
+++ src/stores/hashtagsStore.ts
@@ -1,14 +1,24 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useHashTagsStore = defineStore('hashtag', () => {
-const currentHashId = ref<number | null>(1)
-function setCurrHash(id:number | null){
-    currentHashId.value = id
-}
+export const useHashTagsStore = defineStore("hashtag", () => {
+  const savedHashtag = sessionStorage.getItem("hashtag");
+
+  const currentHashId = ref<number | null>(
+    savedHashtag !== null ? Number(savedHashtag) : 1,
+  );
+
+  function setCurrHash(id: number | null) {
+    currentHashId.value = id;
+    if (id === null) {
+      sessionStorage.removeItem("hashtag");
+    } else {
+      sessionStorage.setItem("hashtag", String(id));
+    }
+  }
 
   return {
-   currentHashId,
-   setCurrHash,
-  }
-})
+    currentHashId,
+    setCurrHash,
+  };
+});
--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -1,21 +1,26 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-import { accentColors } from '../data/colors'
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import { accentColors } from "../data/colors";
 
-export const useThemeStore = defineStore('theme', () => {
-   
-const currentTheme = ref<string>(accentColors[0]!)
-function setCurrTheme(color:string){
-    currentTheme.value = color
-     document.documentElement.style.setProperty(
-        "--accent-color",
-        color
-      );
-}
+export const useThemeStore = defineStore("theme", () => {
+  const savedTheme = sessionStorage.getItem("theme");
+  const currentTheme = ref<string>(
+    savedTheme !== null ? savedTheme : accentColors[0]!,
+  );
+  function setCurrTheme(color: string) {
+    currentTheme.value = color;
+    sessionStorage.setItem("theme", color);
+    document.documentElement.style.setProperty("--accent-color", color);
+  }
+
+   document.documentElement.style.setProperty(
+    "--accent-color",
+    currentTheme.value
+  );
 
   return {
     accentColors,
     currentTheme,
-   setCurrTheme
-  }
-})
+    setCurrTheme,
+  };
+});
