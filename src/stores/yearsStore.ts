e47321c5e58f440a375776aac6b8854dff67b511
saashhhaa <aleksandra.bystrova@alatoo.edu.kg>
2026-09-13 23:11:45 +0600

    feat: добавила листание картинок на стрелки клавы

---
+++ src/stores/yearsStore.ts
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { years } from '../data/years'
+
+export const useYearsStore = defineStore('years', () => {
+ const sortedYears = years.sort((a, b) => b - a)
+    const currentYear = ref(sortedYears[0])
+    function computeCurrYear (selectedYear:number){
+        currentYear.value = selectedYear
+    }
+
+  return {
+   sortedYears,
+   currentYear,
+   computeCurrYear
+  }
+})
