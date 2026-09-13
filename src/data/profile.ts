f9f45b87a421d7ddb3eb7488ae0f21330d91a011
saashhhaa <aleksandra.bystrova@alatoo.edu.kg>
2026-09-13 23:12:08 +0600

    feat: текст футера меняется в зависимости от сезона

---
+++ src/data/profile.ts
@@ -0,0 +1,31 @@
+import profile_img from '../assets/images/personal/profile.png'
+
+
+// import profile_img from '../assets/images/personal/profile_ninel.png'
+
+const FOOTER_TEXT = {
+    fall: '｀、ヽ(ノ＞＜)ノ ｀、ヽ｀☂ ',
+    winter: '(｡•́ ︿ •̀｡) ｀、ヽ｀❄',
+    spring: '✿ヽ(°▽°)ノ✿ °｡⋆⸜ 🌸⋆｡°',
+    summer: '(；  ﾉД`) ﾉ  ☀️  '
+}
+
+function getCurrentSeason (){
+    const month = new Date().getMonth() +1
+    if(month >=3 && month <=5 ) return 'spring'
+    else if (month >=6 && month <= 8) return 'summer'
+    else if (month>=9 && month <=11) return 'fall'
+    else return 'winter'
+}
+// const currSeason = FOOTER_TEXT[getCurrentSeason()]
+
+const currSeason = FOOTER_TEXT["summer"]
+
+export const profile = {
+    NICKNAME: "Кир",
+    DESCRIPTION: "(( _ _ ))..zzzZ",
+    IMAGE: profile_img,
+    FOOTER: currSeason
+}
+
+//  \n [] <- ебейшая кириешка
